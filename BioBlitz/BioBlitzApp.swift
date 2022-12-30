f8c0e9a804eadd4a427b040cabb8c560e708f15b
mitza98 <mihai.cerneanu@gmail.com>
2022-12-30 11:24:26 +0200

    Create layout

---
+++ BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Mihai Cerneanu on 30.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BioBlitz/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  BioBlitz
+//
+//  Created by Mihai Cerneanu on 30.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text ("GREEN: 0")
+                
+                Spacer()
+                
+                Text("BIOBLITZ")
+                
+                Text("RED: 0")
+            }
+            .font(.system(size: 36).weight(.black))
+            
+            VStack {
+                ForEach(0..<11, id: \.self) { row in
+                    HStack {
+                        ForEach (0..<22, id: \.self) { col in
+                            Text("X")
+                        }
+                    }
+                }
+            }
+        }
+        .padding()
+        .fixedSize()
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
