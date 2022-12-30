008f3fd1c7a8e904b0344272ff483fd3e9e687c5
mitza98 <mihai.cerneanu@gmail.com>
2022-12-30 11:45:19 +0200

    Create Bacteria class and view

---
+++ BioBlitz/BacteriaView.swift
@@ -0,0 +1,20 @@
+//
+//  BacteriaView.swift
+//  BioBlitz
+//
+//  Created by Mihai Cerneanu on 30.12.2022.
+//
+
+import SwiftUI
+
+struct BacteriaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BacteriaView_Previews: PreviewProvider {
+    static var previews: some View {
+        BacteriaView()
+    }
+}
